35d913fdbac2ad582ae4868c63fd07fabfe5ba90
Metapsyche <mindrogen@gmail.com>
2021-04-09 22:24:11 -0400

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60710af9951877001f1e1bc0",
+				"60710af9951877001f1e1bc2"
+			]
+		},
+		"60710af9951877001f1e1bc0": {
+			"id": "60710af9951877001f1e1bc0",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60710af9951877001f1e1bc2": {
+			"id": "60710af9951877001f1e1bc2",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
